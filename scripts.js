8e674b56b5e0a17aa60b16f02d5025ae62c63daa
ARKLightning06 <64155125+ARKLightning06@users.noreply.github.com>
2026-05-27 22:02:33 -0400

    work on ui for rps, set up flaming text visual, set up images for human and computer, about to replace the setButtonActive function with resetUI function and have the buttons just reset to avoid issues

--- scripts.js
+++ scripts.js
@@ -12,6 +12,19 @@ function setButtonDisabled(isActive) {
     rochambeauButtonUI.disabled = isActive;
 }
 
+function resetUI() {
+    const uiRPSContainer = document.querySelector("#rps-ui-container");
+    const uiScoreboard = document.querySelector("#rps-ui-scoreboard");
+    const uiPlayerSide = document.querySelector("#rps-ui-playerside");
+    const uiMiddleSide = document.querySelector("#rps-ui-middleside");
+    const uiComputerSide = document.querySelector("#rps-ui-computerside");
+    uiPlayerSide.replaceChildren();
+    uiMiddleSide.replaceChildren();
+    uiComputerSide.replaceChildren();
+    uiScoreboard.textContent = "";
+    uiRPSContainer.classList.remove("flaming-text");
+}
+
 
 function playRPSGameText() {
     setButtonDisabled(true);
@@ -65,32 +78,69 @@ function playRPSRoundText() {
 }
 
 function playRPSGameUI() {
+    //deactivate button to avoid calling same function
     setButtonDisabled(true);
+
+    //set score variables
     let playerScore = 0;
     let computerScore = 0;
-    const uiContainer = document.querySelector("#rps-ui-container");
-    const uiScoreboard = document.createElement("div");
-    const uiRPSIcons = document.createElement("div");
-    uiRPSIcons.classList.add("flex-element-horizontal");
-    const rockIcon = document.createElement("img");
-    const paperIcon = document.createElement("img");
-    const scissorsIcon = document.createElement("img");
-    const rpsIcons = [rockIcon, paperIcon, scissorsIcon];
+
+    //find existing divs to manipulate
+    const uiRPSContainer = document.querySelector("#rps-ui-container");
+    const uiScoreboard = document.querySelector("#rps-ui-scoreboard");
+    const uiPlayerSide = document.querySelector("#rps-ui-playerside");
+    const uiMiddleSide = document.querySelector("#rps-ui-middleside");
+    const uiComputerSide = document.querySelector("#rps-ui-computerside");
+
+    //create images
+    const playerRockIcon = document.createElement("img");
+    const playerPaperIcon = document.createElement("img");
+    const playerScissorsIcon = document.createElement("img");
+    const comRockIcon = document.createElement("img");
+    const comPaperIcon = document.createElement("img");
+    const comScissorsIcon = document.createElement("img");
+
+    //initialize arrays to make it easier to manipulate
+    const playerRPSIcons = [playerRockIcon, playerPaperIcon, playerScissorsIcon];
+    const comRPSIcons = [comRockIcon, comPaperIcon, comScissorsIcon];
     const rpsImages = ["./images/rock.jpg", "./images/paper.png", "./images/scissors.jpg"];
-    for (i = 0; i < rpsIcons.length; i++) {
-        let icon = rpsIcons[i];
+    const rpsIDs = ["#player-rock", "#player-paper", "#player-scissors", "#com-rock", "#com-paper", "#com-scissors"];
+
+    //stylize
+    uiScoreboard.textContent = "Welcome to ROCK PAPER SCISSORS!";
+    uiRPSContainer.classList.add("flaming-text");
+    const playerLabel = document.createElement("span");
+    const comLabel = document.createElement("span");
+    const scoreOfPlayer = document.createElement("span");
+    const scoreOfCom = document.createElement("span");
+    playerLabel.textContent = "Player";
+    comLabel.textContent = "Computer";
+    scoreOfPlayer.textContent = playerScore;
+    scoreOfCom.textContent = computerScore;
+    uiPlayerSide.appendChild(playerLabel);
+    uiComputerSide.append(comLabel);
+    uiPlayerSide.appendChild(scoreOfPlayer);
+    uiComputerSide.append(scoreOfCom);
+    for (i = 0; i < playerRPSIcons.length; i++) {
+        let playerIcon = playerRPSIcons[i];
+        let comIcon = comRPSIcons[i]
         let source = rpsImages[i];
-        icon.classList.add(".button-image");
-        icon.src = source;
-        icon.height = 100;
-        icon.width = 100;
-        uiRPSIcons.appendChild(icon);
+        playerIcon.classList.add("button-image");
+        comIcon.classList.add("button-image");
+        playerIcon.classList.add("unselected-button");
+        comIcon.classList.add("unselected-button");
+        playerIcon.setAttribute("id", rpsIDs[i]);
+        comIcon.setAttribute("id", rpsIDs[i + 3]);
+        playerIcon.src = source;
+        comIcon.src = source;
+        playerIcon.height = 100;
+        playerIcon.width = 100;
+        comIcon.height = 100;
+        comIcon.width = 100;
+        uiPlayerSide.appendChild(playerIcon);
+        uiComputerSide.appendChild(comIcon);
     }
-    uiContainer.appendChild(uiRPSIcons);
-    
-
-
+    uiMiddleSide.textContent = "VS.";
     setButtonDisabled(false);
-
     
 }
